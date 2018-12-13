711352d43ad91cd3f8ee3e2957108df91a28cd7f
lostroom <mi.agarkov@gmail.com>
2018-12-13 18:44:20 +0300

    1

--- manage.py
+++ manage.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?11"
+            "forget to activate a virtual environment?1122"
         ) from exc
     execute_from_command_line(sys.argv)
